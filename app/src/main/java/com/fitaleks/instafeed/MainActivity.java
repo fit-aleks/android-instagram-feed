3122fece5b1cec336b09c6f1c34e4ec7502e3627
Alexander Kulikovskiy <fit.aleks@gmail.com>
2015-08-23 16:41:55 +0300

    Client id changed and removed

--- app/src/main/java/com/fitaleks/instafeed/MainActivity.java
+++ app/src/main/java/com/fitaleks/instafeed/MainActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String INSTA_CLIENT_ID      = "a28e7b8f15a449408fca85962bf7e4a2";
+    public static final String INSTA_CLIENT_ID      = "enter_your_key_here";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
